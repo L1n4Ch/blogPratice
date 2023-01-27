2de9bc40b71c039a2ce203834aee61fd35e48a53
lsc <lin337982849@gmail.com>
2023-01-28 04:03:02 +0800

    整理代码注释以及复习代码逻辑

--- blog-parent/blog-api/src/main/java/com/lsc/blog/handler/AllExceptionHandler.java
+++ blog-parent/blog-api/src/main/java/com/lsc/blog/handler/AllExceptionHandler.java
@@ -5,12 +5,12 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-// 对加了Controller注解的方法进行拦截处理-AOP的实现
+// @ControllerAdvice对加了Controller注解的方法进行拦截处理-AOP的实现
 @ControllerAdvice
 public class AllExceptionHandler {
-    // 进行异常处理，处理Exception.class的异常。
+    // @ExceptionHandler进行异常处理，处理Exception.class的异常。
     @ExceptionHandler(Exception.class)
-    // 加上去返回json数据，控制台preview才会显示"-999 系统异常"
+    // @ResponseBody 加上去返回json数据，控制台preview才会显示"-999 系统异常"
     @ResponseBody
     public Result doException(Exception ex){
         // 后续要加上日志处理
