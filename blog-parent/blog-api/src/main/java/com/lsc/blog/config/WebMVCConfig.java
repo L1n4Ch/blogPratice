e6d5949a8902e3e37efbd7ac5c29a0796eddfa27
L1n4Ch <lin337982849@gmail.com>
2023-02-22 17:20:49 +0800

    修改前端端口

--- blog-parent/blog-api/src/main/java/com/lsc/blog/config/WebMVCConfig.java
+++ blog-parent/blog-api/src/main/java/com/lsc/blog/config/WebMVCConfig.java
@@ -17,7 +17,9 @@ public class WebMVCConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         // 前端为8080端口，后端为8888端口，跨域配置即解决8080可以访问8888端口的所有接口服务，（/**：即所有接口）
-        registry.addMapping("/**").allowedOrigins("http://localhost:8080");
+//        registry.addMapping("/**").allowedOrigins("http://localhost:8080");
+        registry.addMapping("/**").allowedOrigins("http://localhost:8887");
+//        registry.addMapping("/**").allowedOrigins("http://116.205.139.13:8887");
     }
 
     // 登录拦截器
