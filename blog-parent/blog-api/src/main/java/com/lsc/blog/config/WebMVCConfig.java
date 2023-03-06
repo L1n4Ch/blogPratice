ee3b379520cd2b4ec391df9c1b4f815ee2662f4f
L1n4Ch <lin337982849@gmail.com>
2023-03-06 08:35:17 +0800

    解决跨域问题（部署）（addCorsMappings---->CorsFilter）

--- blog-parent/blog-api/src/main/java/com/lsc/blog/config/WebMVCConfig.java
+++ blog-parent/blog-api/src/main/java/com/lsc/blog/config/WebMVCConfig.java
@@ -7,21 +7,21 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
+//@Configuration
 public class WebMVCConfig implements WebMvcConfigurer {
 
     @Autowired
     private LoginInterceptor loginInterceptor;
 
     // 跨域配置
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        // 前端为8080端口，后端为8888端口，跨域配置即解决8080可以访问8888端口的所有接口服务，（/**：即所有接口）
-//        registry.addMapping("/**").allowedOrigins("http://localhost:8080");
-//        registry.addMapping("/**").allowedOrigins("http://116.205.139.13:8887");
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8887","http://116.205.139.13:8887","blog.cmjlkj.com:8887");
-    }
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        // 前端为8080端口，后端为8888端口，跨域配置即解决8080可以访问8888端口的所有接口服务，（/**：即所有接口）
+////        registry.addMapping("/**").allowedOrigins("http://localhost:8080");
+////        registry.addMapping("/**").allowedOrigins("http://116.205.139.13:8887");
+//        registry.addMapping("/**")
+//                .allowedOrigins("http://localhost:8887","http://116.205.139.13:8887","blog.cmjlkj.com:8887");
+//    }
 
     // 登录拦截器
     @Override
